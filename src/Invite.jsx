8a5ddb0774a6f474eb290b299add31d78ccb5d1d
Darlington <obidarlington09@gmail.com>
2024-07-17 17:32:30 +0100

    Few tweeks

--- src/Invite.jsx
+++ src/Invite.jsx
@@ -12,6 +12,22 @@ export const Invite = () => {
               alt=""
             />
           </div>
+          <div className="invite-text">
+            <h3>invite a friend</h3>
+            <p>5,000 🪙 for you and your friend</p>
+          </div>
+        </div>
+        <div className="invite-box">
+          <div className="invite-image-part">
+            <img
+              src={require("./Images/image-removebg-preview (1).png")}
+              alt=""
+            />
+          </div>
+          <div className="invite-text">
+            <h3>invite a friend</h3>
+            <p>5,000 🪙 for you and your friend</p>
+          </div>
         </div>
       </div>
       <button className="invite-btn">invite</button>
